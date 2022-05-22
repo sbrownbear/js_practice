a72041cd71b6c846058e0590fe0a88c851f537fd
Sergey Konopev <74677832+sbrownbear@users.noreply.github.com>
2022-05-22 19:11:39 +0300

    Update js_hw_3.js

--- js_hw_3.js
+++ js_hw_3.js
@@ -9,7 +9,7 @@ const checkAge = function(age) {
     age = Number(age)
     if(!isNaN(age)) {
         if (age < age_2){
-            console.log("You don’t have access cause your age is " + age + " It’s less then " + age_2)
+            console.log("You don't have access cause your age is " + age + " It's less then " + age_2)
         } else if(age >=  age_2 && age <  age_3) {
             console.log("Welcome  !")
         } else if(age  > age_3) {
